9ab431797227e29b8a30b44b245437861aef8a1b
0magical <marilynventura@outlook.com>
2022-10-23 05:33:26 -0400

    made local storage work

---
+++ Develop/index.js
@@ -0,0 +1,13 @@
+var hrs = [hr9, hr10, hr11, hr12, hr13, hr14, hr15, hr16, hr17]
+var currentDay = $('#currentDay')
+
+function begin() {
+   
+    currentDay.text(moment().format('MMM Do YY'))
+
+
+
+
+}
+begin()
+
