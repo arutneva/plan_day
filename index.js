3247b805cdd5cc003251c46140ca419ed8a4f1c5
0magical <marilynventura@outlook.com>
2022-10-28 19:16:13 -0400

    added for loop

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
-var hrs = [hr9, hr10, hr11, hr12, hr13, hr14, hr15, hr16, hr17]
-var currentDay = $('#currentDay')
+let hrs = [hr9, hr10, hr11, hr12, hr13, hr14, hr15, hr16, hr17]
+let currentDay = $('#currentDay')
+let currentHour = today.hour();
 
-function begin() {
-   
+
+function displayDay() {
+
     currentDay.text(moment().format('MMM Do YY'))
 
-
-
-
 }
-begin()
 
+function timeColors() {
+    for (let i = 0; i < hrs.length; i++) {
+        if (currentHour > hrs[i]){
+            $('#' + hrs[i]).addClass('past')
+        }else if (currentHour == hours[index]){
+            $('#' + hrs[i]).addClass('present')
+        }else if (currentHour < hours[index]){
+            $('#' + hrs[i]).addClass('future')
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+displayDay()
+timeColors()
